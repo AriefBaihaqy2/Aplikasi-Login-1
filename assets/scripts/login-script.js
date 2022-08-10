395f657d873ab10766f3c216f98660493fc7c6da
AriefBaihaqy2 <110958395+AriefBaihaqy2@users.noreply.github.com>
2022-08-10 12:44:18 +0700

    Update login-script.js
    Mengganti komentar sesuai format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,64 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/**
+* Membuat variabel loginFormElement untuk tmpilan form
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+
+/**
+* Membuat variabel inputEmailElement untuk tampilan from
+* @constant {HTMLElement} 
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/**
+* Membuat tampilan inputPassword Element
+* @constant {HTMLElement} 
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+* Membuat variabel expectedEmail untuk menyimpan informasi email
+* @constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+
+/**
+* Membuat variabel expectedPassword untuk menyimpan informasi password
+* @constant {string}
+*/
 const expectedPassword = 'superpassword';
 
-loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+// Menambahkan aksi klik pada button Submit
+loginFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    /**
+    * Membuat variabel email untuk menyimpan nilai email
+    * @constant {string}
+    */
+    const email = inputEmailElement.value;
+
+
+    /**
+    * Membuat variabel password untuk menyimpan nilai password
+    * @constant {string}
+    */
+    const password = inputPasswordElement.value;
+    
+    // Membuat logika perbandingan untuk memanggil informasi email dan password
+    if (email == expectedEmail && password == expectedPassword) {
+
+      // Jika email dan password sudah sesuai, maka akan menuju halaman Home
+      goToHome();
+    } else {
+      
+      // Jika salah satu atau kedua input tidak sesuai, akan menampilkan PopUp error
+      showPopUp();
+    }
 });
